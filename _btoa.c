077d4de31572bf9bd392bf176e343bfa007f5fc9
Abdallah-Abdelrahman <abdallah_alkaser@outlook.com>
2023-09-18 02:29:56 +0300

    Save btoa changes

--- _btoa.c
+++ _btoa.c
@@ -14,11 +14,17 @@ char *_btoa(unsigned int n)
 
 	ptr = _realloc(ptr, 0, BUFF);
 
+	if (n == 0)
+	{
+		ptr[i] = 48;
+		ptr[i + 1] = 0;
+		ptr = _realloc(ptr, BUFF, i + 2);
+		return (ptr);
+	}
 	for (; n > 0; i++, n /= 2)
 		ptr[i] = (n % 2) + 48;
-	ptr[i] = (n % 2) + 48;
-	ptr[i + 1] = 0;
-	ptr = _realloc(ptr, BUFF, i + 2);
+	ptr[i] = 0;
+	ptr = _realloc(ptr, BUFF, i + 1);
 
 	_rev_string(ptr);
 	return (ptr);
